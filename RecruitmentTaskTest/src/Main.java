7a8b60fcffe6ba7c9e15977b5bcdc60e707aedf6
hitstrelok <hitstrelok@gmail.com>
2023-11-07 23:03:44 +0100

    Fixed the error with highest pair, Valentyn Horanskyi 63500

--- RecruitmentTaskTest/src/Main.java
+++ RecruitmentTaskTest/src/Main.java
@@ -1,31 +1,26 @@
-public class Main {
-    public static void main(String[] args) {
-        int numberMax=0;
-        int numberMin=100;
-        int[] tab1 = {3, 2, -2, 5, -3};
-        int[] tab2 = {1, 1, 2, -1, 2, -1};
-        int[] tab3 = {1, 2, 3, -4};
-        int[] tab4 = {1, 1};
+ public class Main {
+        public static void main(String[] args) {
+            int numberMax = 0;
+            int[] tab1 = {2, -5, -3, 5, 3, -2};
 
-        for(int i=0; i<tab1.length;i++ ) {
-
-            if(tab1[i]<numberMin){
-                numberMin=tab1[i];
+            for (int t = 0; t < tab1.length; t++) {
+                for (int j = 0; j < tab1.length; j++) {
+                    if (tab1[t] > numberMax && tab1[t] == Math.abs(tab1[j]) && tab1[j]<0) {
+                        numberMax = tab1[t];
+                    }
+                }
             }
-            if(tab1[i]>numberMax){
-                numberMax=tab1[i];
-            }
-            for (int k : tab1) {
-                        if ((Math.abs(tab1[i]) == Math.abs(k) && tab1[i] != k) && tab1[i] >= 0) {
-                            System.out.println(tab1[i]);
-                            System.out.println(k);
-                            return;
-                        }
+            for (int i = 0; i < tab1.length; i++) {
+                for (int k : tab1) {
+                    if ((Math.abs(tab1[i]) == Math.abs(k) && tab1[i] != k) && tab1[i] >= 0 && Math.abs(tab1[i])==numberMax) {
+                        System.out.println(tab1[i]);
+                        System.out.println(k);
+                        return;
+                    }
 
                 }
             }
             System.out.println("lack of pair");
-            return;
         }
 
 }
